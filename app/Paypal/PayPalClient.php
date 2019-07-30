2334bd1b2b106f5e843325de3d51b64cf5c13414
Kanrmutl <kan4227@gmail.com>
2019-07-30 08:33:11 +0700

    Sandbox To Production (Live Client)
    เปลี่ยนจากการซื้อผ่านบัญชีทดสอบเป็นการซื้อขายจริง

--- app/Paypal/PayPalClient.php
+++ app/Paypal/PayPalClient.php
@@ -3,7 +3,8 @@
 namespace App\Paypal;
 
 use PayPalCheckoutSdk\Core\PayPalHttpClient;
-use PayPalCheckoutSdk\Core\SandboxEnvironment;
+// use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Core\ProductionEnvironment;
 
 class PayPalClient
 {
@@ -14,10 +15,17 @@ class PayPalClient
 
     public static function environment()
     {
-        $clientId ="AdeKmKTa4nB9aWe7oZbheexKhuDFwXkfJIwjf8qcbdVlek869ZnqJ34TvF-rUvJcjVSRcXh26ff7VdDk";
-        $clientSecret ="EFO4pLI-5AN46UqSEYLaShkveQKgig8X1B0wCv5PHB2XjzUb4VbRBXGml9zdFeqS02U0ygTDzzuj3U8A";
-       // This sample uses SandboxEnvironment. 
-       // In production, use ProductionEnvironment.
-        return new SandboxEnvironment($clientId, $clientSecret);
+        // -------------------  live ENV -------------------
+        // In production, use ProductionEnvironment.
+        $clientId ="AX33yAYWKaL0h0LiSN6tyir0CGD_Ft9ztwCP2xqhQabpK8CfakgrLgPdD1TKZd16YWL6W7FINMAK1j_7";
+        $clientSecret ="EFO4pLI-ENYIHdfig_V_7tdyQYDZzt_g7ZSRQqEYAhTVgYG0rHavQqM3FZyYu66WZWDGO8VHW-qrqQ8tqQUiHD-C";
+        return new ProductionEnvironment($clientId, $clientSecret);
+
+        // ----------- Sandbox ENV -----------
+        // This sample uses SandboxEnvironment. 
+        // $clientId ="AdeKmKTa4nB9aWe7oZbheexKhuDFwXkfJIwjf8qcbdVlek869ZnqJ34TvF-rUvJcjVSRcXh26ff7VdDk";
+        // $clientSecret ="EFO4pLI-5AN46UqSEYLaShkveQKgig8X1B0wCv5PHB2XjzUb4VbRBXGml9zdFeqS02U0ygTDzzuj3U8A";
+        // return new SandboxEnvironment($clientId, $clientSecret);
+        
     }
 }
