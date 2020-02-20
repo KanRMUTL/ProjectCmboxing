daeda2d1aa8e8c4fe06be5f794fa5c1834ad5013
Kiadtisak Lowongsa <kan4227@gmail.com>
2020-02-20 17:01:18 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    // public function boot(UrlGenerator $url)
-    public function boot()
+    public function boot(UrlGenerator $url)
+    // public function boot()
     {
         Schema::defaultStringLength(191);
         env('APP_ENV') === 'production' ? $url->forceScheme('https') : '' ;// Environment checking
